7b1830cded066ae269659d212202b84fff79f194
Nihas Kalam <nihasna@gmail.com>
2023-03-25 11:15:23 +0000

    radix sort

--- app.js
+++ app.js
@@ -1,33 +1,70 @@
-function pivot(arr, start = 0, end = arr.length - 1) {
-  function swap(arr, i, j) {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-  }
-  let pivot = start;
-
-  let swapIdx = start;
-  for (let i = start + 1; i <= end; i++) {
-    if (arr[i] < arr[pivot]) {
-      swapIdx++;
-      swap(arr, swapIdx, i);
-    }
-  }
-
-  swap(arr, pivot, swapIdx);
-  return swapIdx;
+function getDigitBasedOnString(num, place) {
+  let str = num.toString();
+  let result = str[str.length - 1 - place];
+  console.log(result);
+  return result;
 }
 
-function quickSort(arr, left = 0, right = arr.length - 1) {
-  if (left < right) {
-    let pivotIndex = pivot(arr, left, right); //3
-    //left
-    quickSort(arr, left, pivotIndex - 1);
-    //right
-    quickSort(arr, pivotIndex + 1, right);
+function digitCountBasedOnString(num) {
+  return num.toString().length;
+}
+
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
+}
+
+function digitCount(num) {
+  if (num === 0) return 1;
+  return Math.floor(Math.log10(Math.abs(num))) + 1;
+}
+
+function mostDigits(arr) {
+  let maxDigits = 0;
+  for (let i = 0; i < arr.length; i++) {
+    maxDigits = Math.max(maxDigits, digitCount(arr[i]));
   }
+  return maxDigits;
+}
+
+function radixSort(arr) {
+  console.log("initial array", arr);
+  let maxDigitCount = mostDigits(arr);
+  console.log("most digits", maxDigitCount);
+
+  for (let i = 0; i < maxDigitCount; i++) {
+    // let bucket = [[], [], [], [], [], [], [], [], [], []];
+    let bucket = Array.from({ length: 10 }, () => ["Hello"]);
+
+    for (let j = 0; j < arr.length; j++) {
+      let digit = getDigit(arr[j], i);
+      bucket[digit].push(arr[j]);
+    }
+    console.log("bucket", bucket);
+
+    // Spread operator adds each elemnents of subarrays the to arr
+    arr = [].concat(...bucket);
+    // If we don't use spread operator, the bucket array will be added to the arr
+    //  with its subarrays
+    arr = [].concat(bucket);
+
+    //   while (arr.length > 0) {
+    //     arr.pop();
+    //   }
+    //   console.log("popped array final", arr);
+
+    //   console.log("bucket length", bucket.length);
+    //   for (let k = 0; k < bucket.length; k++) {
+    //     let subBucket = bucket[k];
+    //     console.log("subbucket", subBucket);
+    //     for (let l = 0; l < subBucket.length; l++) {
+    //       arr.push(subBucket[l]);
+    //     }
+    //   }
+    //   console.log("pushed array", arr);
+  }
+
   return arr;
 }
 
-let arr = quickSort([4, 8, 2, 1, 5, 7, 6, 3]);
-console.log(arr);
+let sorted = radixSort([3, 250, 2, 1, 115, 10, 1000]);
+console.log(sorted);
